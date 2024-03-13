ca3526f2bf4ec1de5ba4c8b6c8fb40f85c79091c
Fazla Hasan <fazlahasant@gmail.com>
2024-03-13 15:45:21 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,16 @@
+const country = 'Bangladesh';
+const division = "Newkhale";
+const district = `B-Baria`;
+// const thana = new String('Demra');
+
+// console.log(typeof thana);
+
+const numbers = [54, 25, 58, 47, 75];
+console.log(numbers);
+// console.log(numbers.length);
+// // console.log(numbers[4])
+// numbers
+
+// const capital = 'Dhaka';
+// console.log(capital.length);
+// console.log(capital[0])
